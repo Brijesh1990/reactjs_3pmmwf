dbeba40f5f4b46f9a081158479863b02c6f42d1f
Brijesh1990 <bkpandey.pandey@gmail.com>
2024-08-23 18:13:28 +0530

    23-08-2024 all data updated

---
+++ module3/hello-world-app/src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from 'react-dom/client'
+const root=ReactDOM.createRoot(document.getElementById("demo"));
+root.render(
+<h1 align='center' style={{color:"red",fontSize:"228px"}}>Hello world</h1>
+)
