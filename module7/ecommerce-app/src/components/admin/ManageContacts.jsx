65b1063f86a713d571a5bf4e3db05eafa42c2a55
Brijesh1990 <bkpandey.pandey@gmail.com>
2024-11-21 15:59:35 +0530

    21-12-2024 all data

--- module7/ecommerce-app/src/components/admin/ManageContacts.jsx
+++ module7/ecommerce-app/src/components/admin/ManageContacts.jsx
@@ -3,14 +3,18 @@ import AdminHeader from './AdminHeader'
 import AdminSidebar from './AdminSidebar'
 import { Table,Button } from 'react-bootstrap'
 import axios from 'axios';
+import { ExportData } from './ExportData';
 export default function ManageContacts() {
 // manage all contact us data in admin panel
 const[data,setData]=useState([]);
+// create a file name 
+const fileName = "contactsdata"; // here enter filename for your excel file
+
 useEffect(()=>{
-    
-    axios.get(`http://localhost:8000/contact-us`).then((response)=>{
-     setData(response.data);
-    })
+
+axios.get(`http://localhost:8000/contact-us`).then((response)=>{
+setData(response.data);
+})
 
 },[data]);
 
@@ -26,49 +30,47 @@ return (
 {/* dashboard content */}
 {/* admin dashboard content */}
 <div className="admin-dashboard col-md-9 p-3">
-<h2 className="ms-3 p-4 mt-3">Manage All Contacts Us Form Data here <button className='btn btn-lg btn-danger float-end'>Export Data in CSV <span className='bi bi-file-excel'></span></button></h2>
+<h2 className="ms-3 p-4 mt-3">Manage All Contacts Us Form Data here  <ExportData apiData={data} fileName={fileName} /></h2>
 <hr className="border border-1 border-dark w-50 ms-5" align="left" />
 {/* manage category here */}
 <Table className='table table-responsive'>
-    <thead>
-        <tr>
-            <th>#Id</th>
-            <th>Name</th>
-            <th>Email</th>
-            <th>Phone</th>
-            <th>Subjects</th>
-            <th>Messages</th>
-            <th>Action</th>
-        </tr>
-    </thead>
-    <tbody>
-      
-      {/* fetch all data of api  */}
-      {data && data.map((item)=>{
-        return (
-            <>
-            <tr>
-            <td>{item.id}</td>
-            <td>{item.name}</td>
-            <td>{item.email}</td>
-            <td>{item.phone}</td>
-            <td>{item.subject}</td>
-            <td>{item.message}</td>
-            <td><div style={{width:"200px"}}><Button className='btn btn-danger btn-sm'><span className='bi bi-trash text-white'></span></Button>
-            | <Button className='btn btn-primary btn-sm' title="click to whatsapp Me"><span className='bi bi-whatsapp text-white'></span></Button>
-            </div>
-            </td>
-        </tr>
-            </>
-        )}   
-        )
-        
-        }
-      
-    </tbody>
+<thead>
+<tr>
+<th>#Id</th>
+<th>Name</th>
+<th>Email</th>
+<th>Phone</th>
+<th>Subjects</th>
+<th>Messages</th>
+<th>Action</th>
+</tr>
+</thead>
+<tbody>
+
+{/* fetch all data of api  */}
+{data && data.map((item)=>{
+return (
+<>
+<tr>
+<td>{item.id}</td>
+<td>{item.name}</td>
+<td>{item.email}</td>
+<td>{item.phone}</td>
+<td>{item.subject}</td>
+<td>{item.message}</td>
+<td><div style={{width:"200px"}}><Button className='btn btn-danger btn-sm'><span className='bi bi-trash text-white'></span></Button>
+| <Button className='btn btn-primary btn-sm' title="click to whatsapp Me"><span className='bi bi-whatsapp text-white'></span></Button>
+</div>
+</td>
+</tr>
+</>
+)}   
+)
+
+}
+
+</tbody>
 </Table>
-
-
 
 </div>
 </div>
